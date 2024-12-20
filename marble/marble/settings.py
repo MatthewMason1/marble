92041ce1f9fb6e8f02a88d497e6fd0f7d0a710d6
Matthew Mason <100880981+MatthewMason1@users.noreply.github.com>
2024-12-20 01:43:02 +0000

    JAJA PLS

--- marble/marble/settings.py
+++ marble/marble/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marble.posts',  # This will now reference the correct app path
+    'posts',  # Correct reference to your app
 ]
 
 
