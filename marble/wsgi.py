5633991ff9d81b0efe9067ae4bc91922e9dd8b50
Matthew Mason <100880981+MatthewMason1@users.noreply.github.com>
2024-12-20 01:08:33 +0000

    HOPE

--- marble/wsgi.py
+++ marble/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marble.settings")
+# Point to the settings module inside the marble subfolder
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marble.marble.settings")
 
 application = get_wsgi_application()
